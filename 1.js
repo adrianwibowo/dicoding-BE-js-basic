3bf1b49492495ffd88d4db1bf8e037da99a675be
Laurensius Adrian Wibowo <elaawers@gmail.com>
2021-05-05 01:17:48 +0700

    [law] initial commit

---
+++ 1.js
@@ -0,0 +1,35 @@
+// function scoreChecker(score) {
+//     let result;
+  
+//     // TODO
+//       if(score >= 60 && score <= 69) {
+//           result = 'Anda mendapatkan nilai D.'
+//       }
+//         else if(score >= 70 && score <= 79) {
+//           result = 'Anda mendapatkan nilai C.'
+//       }
+//         else if(score >= 80 && score <= 89) {
+//           result = 'Anda mendapatkan nilai B.'
+//       }
+//         else if(score >= 90) {
+//           result = 'Selamat! Anda mendapatkan nilai A.'
+//       }
+//       else {
+//         result = 'Anda mendapatkan nilai E.'
+//       }
+  
+  
+//     // Jangan hapus kode ini
+//     return result;
+//   }
+
+//   console.log( scoreChecker(59))
+
+
+const numberSet = new Set([1, 6, 4, 4, 1]);
+console.log(numberSet);
+numberSet.add(5);
+numberSet.add(10);
+numberSet.add(6);
+
+console.log(numberSet);
